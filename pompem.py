5d363cb0fe2aec051593085af187485e99c389c3
@paulopatto <paulopatto@gmail.com>
2014-03-27 23:05:47 -0300

    [update_command_line_argument_lib] WIP: Adapt to python 2.6 and 2.7.

--- pompem.py
+++ pompem.py
@@ -1,40 +1,103 @@
 # -*- coding: UTF-8 -*-
 
 import sys
-sys.path.insert(0, '..')
-# import optparse
-import argparse
+if sys.version_info > (2, 6):
+    import argparse
+else:
+    import optparse
+
 from Engine.Update import UpdateVersion
 from Engine.ExecAndPrint import execute
 
+sys.path.insert(0, '..')
+
+
+def args_to_legacy():
+    print "Legacy"
+
+
+def args_to_newer():
+    print "Mew"
+
 
 def main():
-    #parser = optparse.OptionParser(add_help_option=False)
+    #TODO: Remove comment.
+    # IF Python < 2.7:
+    parser = optparse.OptionParser(add_help_option=False)
+    # ELSE:
     parser = argparse.ArgumentParser(description='Explot find tool')
 
-    parser.add_option("-s", "--search", dest="keywords", type="string",
-                                        help="text for search",)
+    #
+    # Para atender a PEP8 no quesito
+    # http://legacy.python.org/dev/peps/pep-0008/#maximum-line-length
+    # verificar a possibilidade de substituir essa criação de argumentos
+    # com YAML ou JSON.
+    #
 
-    parser.add_option("--txt", dest="fileText", \
-                      action="store_true", help="enter the file name",)
+    #
+    # TODO: Que tal em vez de usar --html e --text usar:
+    # --format com opções [txt|html|json|csv]
+    #
+    parser.add_argument(
+        '-m',
+        '--html',
+        dest='fileHtml',
+        action='store_true',
+        help='entre the file name'
+    )
+    parser.add_argument(
+        '-s',
+        '--search',
+        dest='keywords',
+        type=str,
+        help='text for search'
+    )
+    parser.add_argument(
+        '-t',
+        '--text',
+        dest='fileText',
+        action='store_true',
+        help='entre the file name'
+    )
+    parser.add_argument(
+        '-u',
+        '--update',
+        dest='update',
+        action='store_true',
+        help='upgrade to latest version'
+    )
 
-    parser.add_option("--html", dest="fileHtml", action="store_true", \
-                      help="enter the file name",)
 
-    parser.add_option("--update",
-                  action="store_true", dest="update",
-                  help="upgrade to latest version")
+    # TODO: Remove this after changes.
+    # parser.add_option("-s", "--search", dest="keywords", type="string",
+    #                                     help="text for search",)
 
-    parser.add_option("-h", "--help",
-                      action="store_true", dest="help", help="-h")
-    (options, args) = parser.parse_args()
+    # parser.add_option("--txt", dest="fileText", \
+    #                   action="store_true", help="enter the file name",)
 
-    argsParameters = {}
-    keywords = options.keywords
-    fileText = options.fileText
-    fileHtml = options.fileHtml
-    update = options.update
-    help = options.help
+    # parser.add_option("--html", dest="fileHtml", action="store_true", \
+    #                   help="enter the file name",)
+
+    # parser.add_option("--update",
+    #               action="store_true", dest="update",
+    #               help="upgrade to latest version")
+
+    # parser.add_option("-h", "--help",
+    #                   action="store_true", dest="help", help="-h")
+
+    # (options, args) = parser.parse_args()
+
+    # argsParameters = {}
+    # keywords = options.keywords
+    # fileText = options.fileText
+    # fileHtml = options.fileHtml
+    # update = options.update
+    # help = options.help
+
+    args = parser.parser_args()
+
+
+
     if help:
        printHelpMessage()
        return
